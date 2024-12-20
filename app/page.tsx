02aedf0d4c60e562b146b57764734a095e6c2212
ThomasLacroix1 <144111645+ThomasLacroix1@users.noreply.github.com>
2024-12-20 13:44:03 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <></>
+    <div className="flex flex-col items-center justify-center h-screen gap-2">
+      <h1 className="text-center text-3xl font-bold">Bienvenue sur l'application !</h1>
+      <button className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition"><a href="/dashboard">Login</a></button>
+    </div>
   );
 }
